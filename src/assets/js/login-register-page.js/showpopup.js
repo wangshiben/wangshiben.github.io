91a7571403239979340f5b52ba6d67d5d1ca1463
ben ben <1349769271@qq.com>
2023-12-22 22:58:17 +0800

    pages

--- src/assets/js/login-register-page.js/showpopup.js
+++
@@ -1,5 +0,0 @@
-export function showpopup(_this, popup_content) {
-    _this.popup_content = popup_content;
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "block";
-}
